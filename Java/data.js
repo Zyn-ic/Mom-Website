e7d2841aae3ce3f53e7a66c20cad638b80711241
Zyn <122565354+Zyn-ic@users.noreply.github.com>
2023-12-19 20:28:54 -0600

    java test and UI change

---
+++ Java/data.js
@@ -0,0 +1,74 @@
+var embeddedData = {
+    
+    "Noni":{
+        "description":"It's an overall treatment for sickness that spans from diabities to cancer to pressure and the list goes on.",
+        "ingredients":["Noni", "water", "sugar", "cermaic", "cinimon"],
+        "price":25,
+        "tags":["vegan", "natural", "diabities", "pressure"],
+        "bottle-sizes":{
+            "16oz": 0,
+            "24oz": 25,
+            "32oz": 30
+        } 
+    },
+
+    "Alkaline-tea":{
+        "description":"item description",
+        "ingredients":["sage", "fever grass", "sour-sap leaf", "berbine leaf", "pear leaf", "mango leaf", "guava leaf"],
+        "price":15,
+        "tags":["vegan", "natural", "stones"],
+        "bottle-sizes":{
+            "16oz": 0,
+            "24oz": 10,
+            "32oz": 30
+        }
+    },
+
+    "cooling":{
+        "description":"item description",
+        "ingredients":["bukut leaf", "pyhsic-nut", "lime", "cream of tater"],
+        "price":15,
+        "tags":["vegan", "natural", "diabities"],
+        "bottle-sizes":{
+            "16oz": 0,
+            "24oz": -15,
+            "32oz": -15
+        }
+    },
+
+    "Purge":{
+        "description":"",
+        "ingredients":["piss a bed", "aloe Vera", "Scaganeal", "flay seed", "pyhsic-nut"],
+        "price":25,
+        "tags":["vegan", "natural", "diabities"],
+        "bottle-sizes":{
+            "16oz": -25,
+            "24oz": 0,
+            "32oz": -25
+        }
+    },
+
+    "Detox-Bitters":{
+        "description":"item description",
+        "ingredients":["Billy Web", "Cida-Bark", "Jec-ass Bitters", "Red Clover", "neem"],
+        "price":20,
+        "tags":["vegan", "natural", "diabities"],
+        "bottle-sizes":{
+            "16oz": 0,
+            "24oz": 15,
+            "32oz": -20
+        }
+    },
+
+    "Viagra-men-and-women":{
+        "description":"item description",
+        "ingredients":["bitters", "Noni", "hibiscus"],
+        "price":20,
+        "tags":["vegan", "natural", "diabities"],
+        "bottle-sizes":{
+            "16oz": 0,
+            "24oz": 45,
+            "32oz": null
+        }  
+    }
+}
--- Java/script.js
+++ Java/script.js
@@ -16,13 +16,15 @@ function getDataFromStorage(key) {
 // Function to dynamically format the data into pages
 function formatDataIntoPages(data, itemsPerPage) {
     const pages = [];
-    const totalItems = data.length;
+    const itemKeys = Object.keys(data);
+    const totalItems = itemKeys.length;
     let currentPage = 1;
     let startIndex = 0;
 
     while (startIndex < totalItems) {
         const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
-        const pageItems = data.slice(startIndex, endIndex);
+        const pageItemKeys = itemKeys.slice(startIndex, endIndex);
+        const pageItems = pageItemKeys.map(key => ({ name: key, ...data[key] }));
         pages.push({ page: currentPage, items: pageItems });
         startIndex += itemsPerPage;
         currentPage++;
@@ -31,42 +33,43 @@ function formatDataIntoPages(data, itemsPerPage) {
     return pages;
 }
 
-// Function to fetch JSON data
-async function fetchDataOrInitialize() {
-    try {
-        // Check if data is already in memory or localStorage
-        if (globalData) {
-            return globalData;
+// Function to retrieve data (without fetching)
+function fetchDataOrInitialize() {
+    return new Promise((resolve, reject) => {
+        try {
+            // Check if data is already in memory or localStorage
+            if (globalData) {
+                resolve(globalData);
+                return;
+            }
+
+            // Attempt to get data from localStorage
+            const storedData = getDataFromStorage('jsonData');
+            if (storedData) {
+                globalData = storedData;
+                resolve(globalData);
+                return;
+            }
+
+            // Access the embedded data directly
+            if (typeof embeddedData === 'object') {
+                globalData = embeddedData;
+
+                // Save the data to localStorage for future use
+                saveDataToStorage('jsonData', globalData);
+
+                resolve(globalData);
+            } else {
+                reject(new Error('Error accessing embedded data'));
+            }
+        } catch (error) {
+            console.error('Error fetching or parsing data:', error.message);
+            // You can provide a default or fallback data here if needed
+            reject(error);
         }
-
-        // Attempt to get data from localStorage
-        const storedData = getDataFromStorage('jsonData');
-        if (storedData) {
-            globalData = storedData;
-            return globalData;
-        }
-
-        // Change the URL to the actual location of your JSON file or API endpoint
-        const response = await fetch("data.json");
-
-        // Check if the fetch was successful (status code 200)
-        if (!response.ok) {
-            throw new Error(`Failed to fetch data (Status ${response.status})`);
-        }
-
-        // Parse the JSON response
-        globalData = await response.json();
-
-        // Save the data to localStorage for future use
-        saveDataToStorage('jsonData', globalData);
-
-        return globalData;
-    } catch (error) {
-        console.error('Error fetching or parsing data:', error.message);
-        // You can provide a default or fallback data here if needed
-        return null;
-    }
+    });
 }
+
 
 // Example usage
 document.addEventListener('DOMContentLoaded', async function () {
@@ -81,8 +84,10 @@ document.addEventListener('DOMContentLoaded', async function () {
     const formattedPages = formatDataIntoPages(jsonData, itemsPerPage);
 
     // Save the formatted data for future use
-    saveDataToStorage('formattedPages', formattedPages);
+    //saveDataToStorage('formattedPages', formattedPages);
 
     // Use the formatted data as needed
     console.log(formattedPages);
 });
+
+    
